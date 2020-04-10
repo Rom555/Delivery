a14781d7d09a31d8a2ba30ea63d4eef766916868
Rom555 <romanzubrilin@gmail.com>
2020-04-10 21:53:15 +0300

    4

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const cartButton = document.querySelector('#cart-button'),
+    modal = document.querySelector('.modal'),
+    close = document.querySelector('.close');
+
+cartButton.addEventListener('click', toggleModal);
+
+close.addEventListener('click', toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle('is-open');
+}
+
+new WOW().init();
